524bf102a6b7c514a87bfda28037049352a251a6
Lee Watson <rev@revthefox.co.uk>
2018-10-01 14:20:52 +0100

    Add support for setting dmenu path.
    Allows users to configure the path to dmenu with the `DMENU_PATH` env
    variable to allow support for drop-in alternatives like rofi.

--- src/exec-dmenu.js
+++ src/exec-dmenu.js
@@ -3,9 +3,12 @@ const { exec } = require('child_process')
 module.exports = (...args) => choices =>
   new Promise((resolve, reject) => {
     let choice = ''
+    let dmenu_path = process.env.DMENU_PATH
     const error = []
 
-    const execCommand = `dmenu ${args}`
+    // Use a default of 'dmenu' if not specified in process.env
+    const dmenu_binary = (typeof dmenu_path === 'undefined') ? 'dmenu' : dmenu_path;
+    const execCommand = `${dmenu_binary} ${args}`
     console.debug('$', execCommand)
     const dmenu = exec(execCommand)
     dmenu.stdin.write(choices)
